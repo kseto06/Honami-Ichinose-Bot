98f023cfe2bde568df22e282a3111b25b4296365
Kaden司徒逸希 <127336883+Winterlicia@users.noreply.github.com>
2023-10-10 21:48:06 -0400

    Add Date.js to find dates for !reminder function

---
+++ Date.js
@@ -0,0 +1,11 @@
+const currentDate = new Date();
+
+export function getTomorrowDate() {
+
+    const year = currentDate.getFullYear();
+    const shortened = String(year).substring(2, 4);
+    const month = currentDate.getMonth() + 1; // Months are zero-indexed, so we add 1.
+    const day = currentDate.getDate() + 1; //Get the date tomorrow to give a reminder
+
+    return String(month + '/' + day + '/' + shortened);
+}
